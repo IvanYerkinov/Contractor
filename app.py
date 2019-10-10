27e9aafda9f4a7fb4d11b7d13027d778c459450c
Ivan Yerkinov <darkblackvoid@yahoo.com>
2019-10-10 09:32:16 -0700

    Changed items to be more presentable

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ mycart.drop()
 
 item = {"name": "", "price": 0, "img_url": ""}
 
-items.insert_one({"name": "test1", "price": 15, "img_url": "https://m.media-amazon.com/images/M/MV5BMGZmNGIxMTYtMmVjMy00YzhkLWIyOTktNTExZGFiYjNiNzdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"})
-items.insert_one({"name": "test2", "price": 10, "img_url": "https://vignette.wikia.nocookie.net/ghostbusters/images/8/8d/GhostbustersIIStorybookByScholasticSc01.png/revision/latest?cb=20170222172348"})
-items.insert_one({'name': 'test3', 'price': 5, 'img_url': ''})
+items.insert_one({"name": "Con Air", "price": 30.00, "img_url": "https://m.media-amazon.com/images/M/MV5BMGZmNGIxMTYtMmVjMy00YzhkLWIyOTktNTExZGFiYjNiNzdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"})
+items.insert_one({"name": "Ghosterbusters II", "price": 35.00, "img_url": "https://vignette.wikia.nocookie.net/ghostbusters/images/8/8d/GhostbustersIIStorybookByScholasticSc01.png/revision/latest?cb=20170222172348"})
+items.insert_one({'name': 'Manos the Hands of Fate', 'price': 25.00, 'img_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Manosposter.jpg/220px-Manosposter.jpg'})
 
 
 def getPrice():
